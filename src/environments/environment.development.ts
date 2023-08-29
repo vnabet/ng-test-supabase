4b25b52c674982a7e0a2fc347fa326284c5a435d
Vincent Nabet <vnabet@groupeisagri.com>
2023-08-29 13:54:21 +0200

    ajout des fichiers d'environement

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://sgkqbsycyfnoewkeqpgp.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNna3Fic3ljeWZub2V3a2VxcGdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMzMDkxODUsImV4cCI6MjAwODg4NTE4NX0.Kyusnj2jrS71FqjLfKprRJLYENSROpEE_-4ktiVh-Yc',
+}
